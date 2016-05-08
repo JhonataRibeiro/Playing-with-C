e6363f0fa17a00d9caa948be26c345f74f935898
= <jhonata.ribeiro.ti@gmail.com>
2016-05-07 23:34:08 -0300

    add some operations

---
+++ bitwise/bitwise_operations.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main(){
+	/*
+		Some bitwise and bitshift operations. 
+	*/
+
+	int i = 9;
+	
+	/*
+		Bitwise: Exclusive OR
+		The following operation perform a logical exclusive OR on each pair of corresponding bits
+	*/
+	
+	i^=15;
+	printf(" Operator bitwise(Eclusive OR): %d\n",i);
+
+	/*
+		Bit shift: Right Shift
+		The following operation perform a Right Shift Operation.
+	*/
+	
+	i>>=1;
+	printf(" Operator right shift (>>): %d\n",i);
+
+	/*
+		Bit shift: Left Shift
+		The following operation perform a Left Shift Operation.
+	*/
+
+	i<<=4;
+	printf(" Operator left shift (<<): %d\n",i);
+	return 0;
+}
